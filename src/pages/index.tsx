908e375c77c5545263412fc9a43c7a8e97df9996
faithandjs <okogbofaith@email.com>
2022-10-27 23:52:27 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.scss";
+
+export default function Index() {
+  return (
+    <div className='box '>
+      <ul>
+        <li>
+          <a href=' '>Twitter Link</a>
+        </li>
+        <li>
+          <a href=' '>Zuri Team</a>
+        </li>
+        <li>
+          <a href=' '>Zuri Books</a>
+        </li>
+        <li>
+          <a href=' '>Python Books</a>
+        </li>
+        <li>
+          <a href=' '>Twitter Link</a>
+        </li>
+        <li>
+          <a href=' '>Background Check for Coders</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
