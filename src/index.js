e48fa1efadbf7a6ededc958c8a72f44a8ed4d9e7
faithandjs <okogbofaith@email.com>
2022-11-05 14:12:37 +0100

    refactor; converted code to react js from react ts

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import App from "./App";
 
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
